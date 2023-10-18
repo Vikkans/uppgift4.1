3f2e22f499564cfa42a179dff0c9ab7d9a5f155c
Vikkans <Jakobsenviktor6@gmail.com>
2023-10-18 10:08:33 +0200

    Add project files.

---
+++ ConsoleApp30/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i <= 30; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+
+
+            for (int i = 200; i >= 180; i--)
+            {
+                Console.WriteLine(i);
+
+            }
+
+            for (int i = 1000; i <= 1400; i=i+50 )
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
